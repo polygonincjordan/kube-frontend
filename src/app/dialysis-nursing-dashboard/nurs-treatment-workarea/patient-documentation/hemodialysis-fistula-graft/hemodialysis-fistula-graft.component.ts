36c82999faddb2ef91972925f06960a400d84088
Smit Patel <smit.patel007@outlook.com>
2024-06-10 18:54:06 +0530

    Implemented design  for hemoDialysis Fistula/Graft

---
+++ src/app/dialysis-nursing-dashboard/nurs-treatment-workarea/patient-documentation/hemodialysis-fistula-graft/hemodialysis-fistula-graft.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hemodialysis-fistula-graft',
+  templateUrl: './hemodialysis-fistula-graft.component.html',
+  styleUrls: ['./hemodialysis-fistula-graft.component.scss']
+})
+export class HemodialysisFistulaGraftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
