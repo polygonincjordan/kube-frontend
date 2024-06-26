6a962a5abb0d5e74aa2732cd3ba1a8b1b0e6f50e
Dipak <dipak@shruntech.onmicrosoft.com>
2024-06-26 11:36:11 +0530

    nursing care plan first tab design

---
+++ src/app/nursing-emergency-dashboard/nurs-treatment-workarea/patient-documentation/nursing-care-plans/nursing-care-plans.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nursing-care-plans',
+  templateUrl: './nursing-care-plans.component.html',
+  styleUrls: ['./nursing-care-plans.component.scss'],
+})
+export class NursingCarePlansComponent implements OnInit {
+  tabLabelList = [
+    'Nutrition',
+    'Elimination & Exchange; Diarrhea',
+    'Activity / Rest',
+    'Coping / Strees Tolerance',
+    'Comfort; Acute Pain',
+    'Safety / Protection; Bleending',
+    'Safety / Protection; Tissue Integrity',
+    'Safety / Protection; Fall Risk',
+    'Safety / Protection; Hyperthermia',
+    'Safety / Protection; Hypothermia',
+    'Safety / Protection; Aspiration',
+    'Safety / Protection; Injection',
+    'Activity/Rest; Sleep Pattern Disturbance',
+    'Activity/Rest; Impaired Physical Mobility',
+    'Activity/Rest; Inffective Breathing Patterns',
+    'Comfort; Chest Pain',
+    'Elimination & Exchange; Dehdration',
+    'Elimination & Exchange; Nausea & Vomating',
+    'Safety / Protection; VTE Risk',
+    'Safety / Protection; Disrhythmias',
+    'Safety / Protection; Unstable B.Glucose',
+    'Nutrition; Ineffecive Infant Feeding Pattern',
+    'Nutrition; Electrolyte Imbalance',
+    'Perception / Cognition; Knowledge Deficit',
+  ];
+  selectedTabName: string = 'Nutrition';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  switchTabs(tabName: string) {
+    this.selectedTabName = tabName;
+  }
+}
