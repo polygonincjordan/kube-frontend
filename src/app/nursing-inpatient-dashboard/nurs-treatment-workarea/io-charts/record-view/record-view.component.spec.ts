5f0a290c80bd1363a8d50bda0f9dc27dd6ff3e31
kevin-shruntech <kevin@shruntech.onmicrosoft.com>
2024-07-26 19:11:06 +0530

    1. Nursing-inpatient-dashboard -IOCHART Tab - Complete

---
+++ src/app/nursing-inpatient-dashboard/nurs-treatment-workarea/io-charts/record-view/record-view.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecordViewComponent } from './record-view.component';
+
+describe('RecordViewComponent', () => {
+  let component: RecordViewComponent;
+  let fixture: ComponentFixture<RecordViewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecordViewComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RecordViewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
