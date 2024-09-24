3ddf61ae666f5e1a292ffdb0a82c62b2ad5255cf
dipak <dipak@shruntech.onmicrosoft.com>
2024-09-24 20:20:16 +0530

    naviget the emr

--- src/app/day-case-dashboard/nurs-treatment-workarea/e-prescription/madication-profile-nurs/e-mar-order-nurse/drug-events-admin/drug-events-admin.component.ts
+++ src/app/day-case-dashboard/nurs-treatment-workarea/e-prescription/madication-profile-nurs/e-mar-order-nurse/drug-events-admin/drug-events-admin.component.ts
@@ -461,7 +461,7 @@ export class DrugEventsAdminComponent implements OnInit {
           this.modalRef.hide()
           this.route.queryParams.subscribe((params: ParamMap) => {
             this.router.navigate(['e-prescription'], { queryParams: { ...params, isEmr: 'true' } });
-            window.location.reload();
+            // window.location.reload();
           });
         })
       },
