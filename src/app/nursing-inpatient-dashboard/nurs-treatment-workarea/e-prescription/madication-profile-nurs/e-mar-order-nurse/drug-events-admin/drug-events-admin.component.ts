90d67f3ddd6f8c214c92fa504e55d8069e9f5167
unknown <ammar.abuothman@optimumpartners.co>
2025-11-16 17:46:03 +0300

    Fix Medication title, submited date, and replace button

--- src/app/nursing-inpatient-dashboard/nurs-treatment-workarea/e-prescription/madication-profile-nurs/e-mar-order-nurse/drug-events-admin/drug-events-admin.component.ts
+++ src/app/nursing-inpatient-dashboard/nurs-treatment-workarea/e-prescription/madication-profile-nurs/e-mar-order-nurse/drug-events-admin/drug-events-admin.component.ts
@@ -214,7 +214,7 @@ export class DrugEventsAdminComponent implements OnInit {
         Falnr: new FormControl(item.Events.Falnr),
         Meevtid: new FormControl(item.Events.Meevtid),
         Rbdad: new FormControl(new Date(), Validators.required),
-        Rbtad: new FormControl(item.Events.Pbtad),
+        Rbtad: new FormControl(''),
         Notgiven: new FormControl(true),
         Rdosdif: new FormControl(''),
         Rtimdif: new FormControl(item.Events.Rtimdif),
@@ -383,6 +383,7 @@ export class DrugEventsAdminComponent implements OnInit {
                             Meresp2: parseData.d.Vma !== null ? parseData.d.Vma : "",
                             Meresp1: this.getUserConfigData.VMA,
                             Rdrugdq: this.administratiForm.get('NotAdminister').value.Rdrugdq.length ? this.administratiForm.get('NotAdminister').value.Rdrugdq : `0.000`,
+                            Rbtad: `${this.parseTime(this.administratiForm.get('NotAdminister').value.Rbdad)}`,
                             Rbdad: `${formatDate(this.administratiForm.get('NotAdminister').value.Rbdad, 'YYYY-MM-DD')}T${formatDate(this.administratiForm.get('NotAdminister').value.Rbdad, "HH:mm:ss")}`
                           }
                           const { Quanunit,DosageStr, ...payload } = PayloadData;
