62ec7a4548af1acf4db09de850015e119fa33ef9
abdelrahman-husen-wi <abdelrahman-husen-wi@ABDELRAHMAN-HUS>
2026-01-20 18:35:09 +0300

    fix vitals by make mews default vital , hide delete vital recored button, fix hardcoded of create vital and fix decscription not sent in request body

--- src/app/day-case-dashboard/check-in/er-vitals/er-vitals.component.ts
+++ src/app/day-case-dashboard/check-in/er-vitals/er-vitals.component.ts
@@ -332,8 +332,24 @@ export class ErVitalsComponent implements OnInit {
         "Obsid": "E10989D8963E1EEE81C5B8A334F03E72",
         "ObsidVers": "0000",
         "Addinfo": ""
+      },
+      {
+        "Einri": "",
+        "Valid": "4522139805EB1FD0BC8A25C3E8D7BADE",
+        "ValidVers": "0000",
+        "Bcpid": "C000C29D2E09C1ED9A5D54D616DB4CEDE",
+        "Extid": "MEWS SCORE",
+        "Name": "MEWS Score",
+        "Value": "",
+        "ValueString": "",
+        "UnitTxt": "UnLess",
+        "NormalRange": "0.000 - 0.999",
+        "Origin": "",
+        "Descr": "",
+        "Obsid": "4522139805EB1FD0BC8A25C3E8D79ADE",
+        "ObsidVers": "0000",
+        "Addinfo": ""
       }
-     
     ];
   }
   openModalForErVital(checkinitem,tab?,documentType?) {
@@ -623,7 +639,7 @@ export class ErVitalsComponent implements OnInit {
       "Lfdnr": this.erListSelectedData.Lfdbw,
       "Orgfa": "",
       "Orgpf": "",
-      "Orgdo": this.erListSelectedData?.Orgpf,
+      "Orgdo":  this.erListSelectedData?.Orgpf,
       "Mitarb": this.storageService.getGpart(),
       "Origin": "",
       "Odate": createDate,
@@ -676,7 +692,7 @@ export class ErVitalsComponent implements OnInit {
 
   //create
 
-  CreateVitalList() {
+   CreateVitalList() {
     this.showMaintain = true;
     let createTime = 'PT' + new Date().getHours() + 'H' + new Date().getMinutes() + 'M' + '00S';
     // if (this.vitalListResp.length != 0) {
@@ -689,9 +705,10 @@ export class ErVitalsComponent implements OnInit {
     this.maintainVitalBarForm.controls.Odate.setValue(new Date());
     this.maintainVitalBarForm.controls.Otime.setValue(this.getTime(createTime));
     this.vitalDefaultListResp.forEach(element => {
-      this.addItemForVital(element);
+    this.addItemForVital(element);
     });
-  }
+  } 
+ 
   openAllVitalModal(template: TemplateRef<any>, index) {
     this.selectedIndex = index;
     const config: ModalOptions = {
